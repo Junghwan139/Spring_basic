51562933c66048275bf8c73bf8a55520a701d1d4
Junghwan <balance139@gmail.com>
2023-04-30 13:15:22 +0900

    23.04.29_2

--- src/main/java/com/example/demo/controller/HelloController.java
+++ src/main/java/com/example/demo/controller/HelloController.java
@@ -1,6 +1,7 @@
 package com.example.demo.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
@@ -12,16 +13,19 @@ public class HelloController {
     // http는 국제 통신프로토콜이다. https는 s(secure) 보안이 강화된 통신 프로토콜
     // port란 한 ip내에 여러 프로그램을 구분짓는 단위(프로그램을 구분짓는 유일한 구분자) 집주소가 ip, 각 집의 방문이 port
 
+    // data만을 return할 때는 ResponseBody를 사용한다.
     @GetMapping("hello")
     @ResponseBody
     public String hello(){
         return "hello world";
     }
 
-    @GetMapping("hello2")
-    @ResponseBody
-    public String hello2(){
-        return "hello2 world";
+    // jsp / thymeleaf같은 템플릿엔진을 사용하여 화면을 return할떄에는 responsebody를 사용하면 안된다.
+    // 그리고 Model이라는 객체에 data를 담아 return xxx를 하여 xxxx.html파일로 데이터를 보낸다.
+    @GetMapping("hello-thymeleaf")
+    public String hello2(Model model){
+        model.addAttribute("getdata","hello2 world");
+        return "hello";
     }
 
 
