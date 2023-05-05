74c8458a6bf724703816fe032882422241ddf90d
Junghwan Lim <balance139@gmail.com>
2023-05-05 19:54:04 +0900

    23.05.05

--- src/main/java/com/example/demo/bm/ByeController.java
+++ src/main/java/com/example/demo/bm/ByeController.java
@@ -1,6 +1,7 @@
-package com.example.demo.controller;
+package com.example.demo.bm;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
@@ -13,6 +14,13 @@ public class ByeController {
         return "bye world";
     }
 
+
+    @GetMapping("bye-test1")
+    public String bye_test(Model model){
+        model.addAttribute("getdata","byebye22");
+
+        return "goodbye"; // templates내에서 "goodbye" html을 찾는다
+    }
 
 
 
